d252bb123448357b3dbd00966397c835441cbece
unknown <r.loboplaza@hotmail.com>
2021-05-23 10:23:07 -0300

    agregando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import R2 from './pages/R2';
+import R3 from './pages/R3';
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={R2} />
+            <Route exact path="/R3" component={R3} />
+          </Switch>
+        </Layout>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/R2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/r2.css';
+
+const R2 = () => {
+  return (
+    <div>
+      <h1>R2</h1>
+    </div>
+  );
+};
+
+export default R2;
